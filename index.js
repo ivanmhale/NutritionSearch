1655811bb609ac9ac7f116ec1529d89a80e94ff2
Ivan <ivanmhale@gmail.com>
2018-05-23 02:25:00 -0400

    configuration for heroku

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 require('./routes/index')(app);
 
 app.listen(process.env.PORT || 5000);
